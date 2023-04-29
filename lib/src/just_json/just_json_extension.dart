5d457ef2ccd8c1c6baba385e40d59c278f4c9dd5
안강민 <hkkm612@gmail.com>
2023-04-29 23:38:17 +0900

    task. [JustJson] PrettyJsonString

--- lib/src/just_json/just_json_extension.dart
+++ lib/src/just_json/just_json_extension.dart
@@ -1,4 +1,8 @@
-extension JustJsonExtension on Map<String, dynamic> {
+import 'dart:convert';
+
+typedef JustJson = Map<String, dynamic>;
+
+extension JustJsonExtension on JustJson {
   /// If val is not null, add property.
   void setIfPresent({required String key, dynamic val, dynamic defaultVal}) {
     if (val != null) {
@@ -12,5 +16,15 @@ extension JustJsonExtension on Map<String, dynamic> {
   /// Else return defaultValue.
   dynamic getIfPresent({required String key, dynamic defaultVal}) {
     return containsKey(key) ? this[key] ?? defaultVal : defaultVal;
+  }
+}
+
+extension JustJsonConverterExtension on JustJson {
+  /// Make pretty Json with Indent.
+  String toPrettyJsonString({int indentSize = 2}) {
+    JsonEncoder encoder = JsonEncoder.withIndent(' ' * indentSize);
+    String prettyJson = encoder.convert(this);
+
+    return prettyJson;
   }
 }
